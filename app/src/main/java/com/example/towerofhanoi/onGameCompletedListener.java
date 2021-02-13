e385d666cdd5bcaa0201262357d5765a119cfa5d
@vng011 <vng011@uit.no>
2021-02-13 04:57:07 +0100

    New layout + new codes

--- app/src/main/java/com/example/towerofhanoi/onGameCompletedListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.towerofhanoi;
-
-//Custom listener to inform GameActivity when the puzzle has been solved.
-public interface onGameCompletedListener {
-        void onGameComplete(boolean isOptimal, int moves, int numDisks);
-
-}
--- app/src/main/java/com/example/towerofhanoi/onGameStartedListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.towerofhanoi;
-
-//Custom listener to inform GameActivity when user has made the first move.
-public interface onGameStartedListener {
-        void onGameStarted();
-}
